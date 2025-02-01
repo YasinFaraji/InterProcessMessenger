aa7a961e1732e00a88b6486a2dbb6412ef804477
YasinFaraji <yasinfaraji100@gmail.com>
2025-02-01 23:51:55 +0330

    Implement runReceiver function in receiver source file

---
+++ src/receiver.cpp
@@ -0,0 +1,31 @@
+#include "receiver.h"
+
+#include <iostream>
+#include <unistd.h>
+#include "common.hpp"
+#include "shared_memory.h"
+#include "synchronization.h"
+
+namespace IPC
+{
+void runReceiver()
+{
+    log("Receiver started");
+    SharedMemory shm;
+    Synchronization sync;
+
+    while (true)
+    {
+        sync.lock();
+        int counter = shm.read();
+        if (counter >= 10)
+            break;
+        shm.write(counter + 1);
+        log("Receiver incremented counter: " + std::to_string(counter + 1));
+        sync.unlock();
+        sleep(1);
+    }
+
+    log("Receiver finished");
+}
+} // namespace IPC
